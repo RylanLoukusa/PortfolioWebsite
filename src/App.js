d40a2b210dd8c2c4cff43c66e635007ddf3e7df2
RylanLoukusa <rylanloukusa@gmail.com>
2023-07-12 17:55:02 -0500

    Add / to url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/portfolio-website" element={<Layout />}>
+        <Route path="/portfolio-website/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
